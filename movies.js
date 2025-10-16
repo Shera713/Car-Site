f0cf7ad8681cdb1b0d1f07f0ed1ef907e0be3e4a
Shera713 <t.sheratoledo@gmail.com>
2025-10-16 11:35:21 -0400

    APi

--- movies.js
+++ movies.js
@@ -15,16 +15,16 @@ movieListEl.innerHTML = moviesData.map(movie => moviesHTML(movie)).join('');
  }
 
  function moviesHTML(movie){
-   return `  
-     <div class="movie">
-      <div class="movie__title">
-        ${movie.title}
-      </div>
-      <p class="movie__body">
-        ${movie.body}
-      </p>
-    </div>
-    ` 
+   return `<div class="movie-card onclick=showMoviePosts(${movie.id})">
+    <div class="movie-card__container">
+<h3>${movie.title}</h3>
+<p><b>year:</b>${movie.year}</p>
+<p><b>imbdID:</b>${movie.imbdID}</p>
+<p><b>movie poster</b> <a href="https://${movie.poster}" target="_blank">
+${movie.poster}
+</a></p>
+</div>
+</div>`
  }
 
  renderMovies(id);
